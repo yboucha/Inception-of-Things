1b85ed31987b64d996dd6e63298f55bb71d9c277
seljama <salma@AAD-83453.localdomain>
2026-02-26 05:58:25 -0800

    changed machines name to yboucha

--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -14,4 +14,4 @@ K3S_TOKEN=$(cat /vagrant/scripts/k3s_token)
 # Install K3s agent and join the server
 #“Connect to Kubernetes server at 192.168.56.110, port 6443.”
 #Port 6443 = Kubernetes API port.
-curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="$K3S_TOKEN" sh -s - agent --node-name sel-jamaSW
+curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="$K3S_TOKEN" sh -s - agent --node-name ybouchaSW
